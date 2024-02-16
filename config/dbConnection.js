624c9d9c2a8372cac68ee707b6c5437fa744f8fd
Abdi <ab_cs2048@yahoo.com>
2024-02-16 15:05:42 +0300

    just some changes

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ const {default: mongoose} = require('mongoose')
 
 const dbConnection = () => {
     try{
-        const conn = mongoose.connect(process.env.MONGODB_URL||5001)
+        const conn = mongoose.connect(process.env.MONGODB_URL)
         console.log('Connection Successful')
     }
     catch(err){
